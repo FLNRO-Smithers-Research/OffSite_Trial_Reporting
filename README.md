a046a37ab1e21e2eae98c966de15f893191e01ce
Matt McLean <matt@geoguy.ca>
2020-04-17 09:36:21 -0700

    Flaten File Structure

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # OffSite_Trial_Reporting
 Scripts to show statistical and graphical output for species trial data
+
+## Loading Data
--- functions/build_dataframe.R
+++ functions/build_dataframe.R
@@ -1,15 +1,12 @@
-source("tree_position.R")
+source("functions/tree_position.R")
 load_all <- function(dir){
-  tree_dir = paste(dir, "/trees", sep = "")
-  plot_dir = paste(dir, "/plots", sep = "")
-  meta_dir = paste(dir, "/meta", sep = "")
-  climate_dir = paste(dir, "/climate", sep = "")
+  
   all_data <- list("trees" = data.frame(), "plots" = data.frame(), "climate" = data.frame(), "meta" = data.frame())
   
-  all_data$plots = load_plots(plot_dir)
-  all_data$trees = load_trees(tree_dir, all_data$plots)
-  all_data$climate = load_climate(climate_dir)
-  all_data$meta = load_meta(meta_dir)
+  all_data$plots = load_plots(dir)
+  all_data$trees = load_trees(dir, all_data$plots)
+  all_data$climate = load_climate(dir)
+  all_data$meta = load_meta(dir)
   save(all_data, file = "trial_data.Rdata")
 }
 
@@ -18,8 +15,11 @@ load_trees <- function(folder, plots){
   file_list = list.files(folder)
   trees = data.frame()
   for(file in file_list){
-    trees = rbind(trees, read.csv(paste(folder, "/", file, sep = "")))
+    if(grepl("FID_", file)){
+      trees = rbind(trees, read.csv(paste(folder, "/", file, sep = "")))
+    }
   }
+    
   trees[,"N"] <- numeric()
   trees[,"E"] <- numeric()
   trees[,"Zone"] <- character()
@@ -36,7 +36,9 @@ load_plots <- function(folder){
   file_list = list.files(folder)
   plots = data.frame()
   for(file in file_list){
-    plots = rbind(plots, read.csv(paste(folder, "/", file, sep = "")))
+    if(grepl("_plot", file)){
+      plots = rbind(plots, read.csv(paste(folder, "/", file, sep = "")))
+    }
   }
   return(plots)
 }
@@ -45,7 +47,9 @@ load_climate <- function(folder){
   file_list = list.files(folder)
   climate = data.frame()
   for(file in file_list){
-    climate = rbind(climate, read.csv(paste(folder, "/", file, sep = "")))
+    if(grepl("_climate", file)){
+      climate = rbind(climate, read.csv(paste(folder, "/", file, sep = "")))
+    }
   }
   expand_climate = data.frame(ClimateSationID = character(), Date = character(), ClimateVar = character(), Value = character())
   for(var in 3:ncol(climate)){
@@ -60,7 +64,9 @@ load_meta <- function(folder){
   file_list = list.files(folder)
   meta = data.frame()
   for(file in file_list){
-    meta = rbind(meta, read.csv(paste(folder, "/", file, sep = "")))
+    if(grepl("meta", file)){
+      meta = rbind(meta, read.csv(paste(folder, "/", file, sep = "")))
+    }
   }
   return(meta)
 }
