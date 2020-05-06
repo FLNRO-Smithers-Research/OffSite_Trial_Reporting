d567d6677b64dd9c8218613cdad5716d3ec678ae
Matt McLean <matt@geoguy.ca>
2020-05-06 14:35:12 -0700

    Convert error bars to Standard Error

--- functions/climate_summary.R
+++ functions/climate_summary.R
@@ -7,36 +7,37 @@ library(datetime)
 #Returns Summary of all sites suitable for plotting.
 temperature_summary <- function(climate, threshold, threshold_2=NULL, start=NULL, end=NULL){
   if(is.null(start)){
-    start = "1970-01-01"
+    start = "1970-01-01" #Easy beginning date range 
   }
   if(is.null(end)){
-    end = "2970-01-01"
+    end = "2970-01-01" #Arbitrary end date, presumably code will no longer be in use by 2970
   }
   ClimateStationID <- unique(climate[,c("ClimateStationID")])
   site_stats = list()
   for(this_site in ClimateStationID){ #Calculate statistics for each site independantly
     active_climate = climate %>% filter(ClimateStationID == this_site & ClimateVar == "temp" & as.Date(Date) >= as.Date(start) & as.Date(Date) <= as.Date(end) )
     events = find_events(active_climate, threshold)
-    if(length(events) > 0){
+    if(length(events) > 0){ #Make sure events exist for site
       df = as.data.frame(t(matrix(unlist(events), nrow=length(unlist(events[1])))))
       colnames(df) <- c("date", "frost_duration", "frost_magnitude")
       if(!is.null(threshold_2)){
         events2 = find_events(active_climate, threshold_2)
-        if(length(events2) > 0){
+        if(length(events2) > 0){ #Confim that there is data to read
           df2 = as.data.frame(t(matrix(unlist(events2), nrow=length(unlist(events2[1])))))
           colnames(df2) <- c("date", "hard_frost_duration", "hard_frost_magnitude")
           site_stats[[this_site]] <- c(this_site, length(df[["frost_magnitude"]]), max(as.numeric(df[["frost_magnitude"]])), sum(as.numeric(df[["frost_duration"]])), 
                                        length(df2[["hard_frost_magnitude"]]), sum(as.numeric(df2[["hard_frost_duration"]])))
         }
-        else{
-          site_stats[[this_site]] <- t(c(this_site, 0, 0, 0, 0, 0))
+        else{ #If no hard frost event fill with 0
+          site_stats[[this_site]] <- c(this_site, length(df[["frost_magnitude"]]), max(as.numeric(df[["frost_magnitude"]])), sum(as.numeric(df[["frost_duration"]])), 
+                                       0, 0)
         }
       }
       else{
         site_stats[[this_site]] <- c(this_site, length(df[["frost_magnitude"]]), max(as.numeric(df[["frost_magnitude"]])), sum(as.numeric(df[["frost_duration"]])))
       }
     }
-    else{
+    else{ #If no events present fill with 0's, inner if makes sure that dimensions are maintained
       if(!is.null(threshold_2)){
         site_stats[[this_site]] <- t(c(this_site, 0, 0, 0, 0, 0))
       }
@@ -61,20 +62,20 @@ temperature_summary <- function(climate, threshold, threshold_2=NULL, start=NULL
 #Returns list of all events along with date, duration, and magnitude.
 temperature_events <- function(climate, threshold, start=NULL, end=NULL){
   if(is.null(start)){
-    start = "1970-01-01"
+    start = "1970-01-01" #Easy beginning date range 
   }
   if(is.null(end)){
-    end = "2970-01-01"
+    end = "2970-01-01" #Arbitrary end date, presumably code will no longer be in use by 2970
   }
   ClimateStationID <- unique(climate[,c("ClimateStationID")])
   all_events = list()
   for(this_site in ClimateStationID){ #Calculate statistics for each site independantly
     active_climate = climate %>% filter(ClimateStationID == this_site & as.Date(Date) >= as.Date(start) & as.Date(Date) <= as.Date(end))
     events = find_events(active_climate, threshold)
-    if(length(events) > 0){
+    if(length(events) > 0){ #Check that data frame is not empty
       event_df = as.data.frame(t(matrix(unlist(events), nrow=length(unlist(events[1])))))
     }
-    else{
+    else{ #If the dataframe is empty fill with 0's to prevent null errors
       event_df = as.data.frame(t(c(0,0,0,0)))
     }
     colnames(event_df) <- c("Date", "Day of Year", "Duration", "Magnitude")
@@ -124,6 +125,7 @@ find_events <- function(active_climate, threshold){
   }
   #Write any remaining data at end of loop
   if (event_length > 0){
+    #event_date is cast to string as date objects do not survive unlisting
     events[[num_events]] <- c(as.character(event_date), day_of_year, event_length, (as.numeric(magnitude)*(-1)))
   }
   return(events)
